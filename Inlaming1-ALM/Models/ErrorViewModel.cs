b03d05109b17e6721711fbc3c80779dc37ab53de
Hannibal Ftaw <Hannibal_rebok@hotmail.com>
2019-11-12 18:31:04 +0100

    First commit

---
+++ Inlaming1-ALM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Inlaming1_ALM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
