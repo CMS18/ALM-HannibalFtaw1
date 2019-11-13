06d96a21ed710402dbdc1057791d593b7b9c1c81
Hannibal Ftaw <Hannibal_rebok@hotmail.com>
2019-11-13 15:53:39 +0100

    Added Customers, Accounts into a list through a viewmodel.

---
+++ Inlaming1-ALM/Models/Entities/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inlaming1_ALM.Models.Entities
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+}
---
+++ Inlaming1-ALM/Models/Services/BankRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Inlaming1_ALM.Models.Entities;
+
+namespace Inlaming1_ALM.Models.Services
+{
+    public static class BankRepository
+    {
+        public static List<Customer> Customers { get; set; }
+        public static List<Customer> GetCustomers()
+        {
+            return Customers;
+        }
+
+        public static void AddCustomers(List<Customer> customers)
+        {
+            Customers = customers;
+        }
+
+        //public List<Account> Accounts = new List<Account>
+        //{
+        //    new Account {CustomerID = 1, Balance = 2000m, AccountID = 1},
+        //    new Account {CustomerID = 2, Balance = 5000m, AccountID = 2},
+        //    new Account {CustomerID = 3, Balance = 8000m, AccountID = 3},
+        //    new Account {CustomerID = 4, Balance = 7000m, AccountID = 4}
+        //};
+
+    }
+}
